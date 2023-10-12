1dd8a901e35a64a6a1554b4478cead452271fda4
Lawrence Letham <LLetham@LethamLF.com>
2023-10-11 19:07:45 -0600

    print array and fill array with random numbers working. Bubble sort working. Starting on insert sort.

--- arrays/ArrayClass.h
+++ arrays/ArrayClass.h
@@ -1,12 +1,52 @@
 #pragma once
+
+#include <iostream>
+#include <time.h>       // need for ctime
+#include <cstdlib>      // need for srand
+
+
 class ArrayClass
 {
 
 private:
+	int j;
 
-	int intArray[100];	// array of 100 integers.
 
 public:
+	// print out array
+	void printArray(int* array, int arraySize) {
+
+		std::cout << "array:\t";
+		for (int k = 0; k < arraySize; k++) {
+			std::cout << array[k] << "\t";
+		}
+		std::cout << std::endl;
+	}
+
+	/******************************************/
+	// Fill array with random numbers less than maxNumberValue
+	void fillArrayRand(int* array, int arraySize, const int maxNumberValue) {
+
+		std::srand(long int(time(0)));    // seed the random number generator with the time of day
+
+		// fill the array with random numbers in valoe from 0 to 999
+		for (int i = 0; i < arraySize; i++) {
+
+			// rand gets a random number. 
+			// Modulo maxNumberValue to keep values less than maxNumberValue
+			array[i] = std::rand() % maxNumberValue;
+		}
+
+		// verify no value in intArray is greater than maxNumberValue
+		for (int i = 0; i < arraySize; i++) {
+			if (array[i] > maxNumberValue) {
+				std::cout << "array[" << i << "]: " << array[i] << "\t Greater than\t" << maxNumberValue << std::endl;
+			}
+		}
+
+
+
+	}
 
 
 };
--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <time.h>       // need for ctime
 #include <cstdlib>      // need for srand
+#include "ArrayClass.h"
 
 
 // main is the test bed for the arrays created in the classes.
@@ -13,98 +14,99 @@ int main()
     const int midNumberValue = 500;
     const int arraySize = 10;
     int intArray[arraySize] = { 0 };	// array of 100 integers. Initialize to 0.
-    int intArraySort[arraySize] = { 0 };  // an array for performing sorting
+    //int intArraySort[arraySize] = { 0 };  // an array for performing sorting
     int tmp;
 
     long int randNum = 0;
 
-    int grt499 = 0;
+    ArrayClass ac;
 
     
-    /******************************/
-    std::srand(time(0));    // seed the random number generator with the time of day
 
-
-    /*************************************************/
-    // Start of testing the array
-
-    // fill the array with random numbers in valoe from 0 to 999
-    for (int i = 0; i < arraySize; i++) {
-
-        intArray[i] = std::rand() % maxNumberValue;    // rand gets a random number. Modulo 1000 to keep less than 1000
-
-    }
-
-    // verify no value in intArray is greater than 1000
-    for (int i = 0; i < arraySize; i++) {
-        if (intArray[i] > maxNumberValue) {
-            std::cout << "intArray[" << i << "]: " << intArray[i] << "\t Greater than\t" << maxNumberValue << std::endl;
-        }
-    }
-
-    // See of 50% of the values in the array are > 499 to measure randomness of rand()
-    for (int i = 0; i < arraySize; i++) {
-        if (intArray[i] > midNumberValue) {
-            grt499++;
-            std::cout << "intArray[" << i << "]:\t" << intArray[i] << "\tGreater than\t" << midNumberValue << std::endl;
-        }
-    }
-
-    std::cout << grt499 << "\t numbers greater than 499" << std::endl;
-
-    // Sort the numbers in the array. Because 51 are less than 499, the value at intArray[50] after the
-    // sort should be very close to 499.
-    // Copy intArray into intArraySort. Sort on intArraySort.
-    // Do bubbles sort.
-    for (int i = 0; i < arraySize; i++) {
-        intArraySort[i] = intArray[i];
-    }
-
-    // print out array to watch numbers move.
-    std::cout << "array:\t";
-    for (int k = 0; k < arraySize; k++) {
-        std::cout << intArraySort[k] << "\t";
-    }
-    std::cout << std::endl;
-
+    // Bubble sort
+    // 1111111111111111111111111
+    /*******************************************************/
+    //// print out array prior to sort
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
 
     for (int i = 0; i < arraySize; i++) {
         for (int j = i + 1; j < arraySize; j++) {
-            if (intArraySort[i] < intArraySort[j]);
-            tmp = intArraySort[i];
-            intArraySort[i] = intArraySort[j];
-            intArraySort[j] = tmp;
-
-            // print out array to watch numbers move.
-            std::cout << "array:\t";
-            for (int k = 0; k < arraySize; k++) {
-                std::cout << intArraySort[k] << "\t";
+            if (intArray[i] > intArray[j]) {
+                tmp = intArray[i];
+                intArray[i] = intArray[j];
+                intArray[j] = tmp;
             }
-            std::cout << std::endl;
         }
-        // print out array to watch numbers move.
-        std::cout << "array:\t";
-        for (int k = 0; k < arraySize; k++) {
-            std::cout << intArraySort[k] << "\t";
-        }
-        std::cout << std::endl;
-
-
     }
+
+    ac.printArray(intArray, arraySize);
+
+    /*******************************************************/
+    // Insertion sort
+    // 22222222222222222222222222222222
+    /*******************************************************/
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
+
+    //for (int i = 1; i < arraySize; i++) {
+    //    int key = intArray[i];
+    //    int j = i - 1; 
+    //    while (j >= 0 && arraySize[j] > key) {
+    //        arraySize[j + 1] = intArray[j];
+    //        j--;
+    //    }
+
+    //    arraySize[j + 1] = key;
+    //}
+
+    ac.printArray(intArray, arraySize);
+
+
+    /*******************************************************/
+    // Merge sort
+    // 33333333333333333333333333333333333
+    /*******************************************************/
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
+
+
+    ac.printArray(intArray, arraySize);
+
+    /*******************************************************/
+    // Quick sort
+    // 444444444444444444444444444444444
+    /*******************************************************/
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
+
+
+    ac.printArray(intArray, arraySize);
+
+
+    /*******************************************************/
+    // Heap sort
+    // 55555555555555555555555555555555555555555555555
+    /*******************************************************/
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
+
+
+    ac.printArray(intArray, arraySize);
+
+    /*******************************************************/
+    // Count sort
+    // 55555555555555555555555555555555555555555555555
+    /*******************************************************/
+    ac.fillArrayRand(intArray, arraySize, maxNumberValue);
+    ac.printArray(intArray, arraySize);
+
+
+    ac.printArray(intArray, arraySize);
+
 
 
 
     /*************/
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
